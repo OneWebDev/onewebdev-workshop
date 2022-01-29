027bbbba56342153b40730e02e4a2e8aba7fb881
Nathan <nathan.reddy@tamu.edu>
2022-01-29 09:17:28 -0600

    Adding comments

--- v3 - Full Stack Website/app.js
+++ v3 - Full Stack Website/app.js
@@ -1,5 +1,6 @@
 // npm i express morgan lokijs ejs --save
 
+// Import libraries
 const express = require('express')
 const morgan = require('morgan')
 const bodyParser = require('body-parser')
@@ -9,7 +10,6 @@ const bodyParser = require('body-parser')
 const loki = require('lokijs');
 var db = new loki('example.db');
 var jokes = db.addCollection('jokes');
-
 
 // Configure Express Application
 var app = express() 
@@ -24,13 +24,14 @@ app.use(morgan('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
+// Use the ejs templating engine in the views folder
 app.set('view engine', 'ejs');
 
 
 app.post('/upvote-joke', (req, res) => {
     // console.log(req.body)
 
-    // Taken from upvote_joke() function in jokes HTML
+    // [id, joke] Taken from upvote_joke() function in jokes HTML
     var id = req.body.id
     var joke = req.body.joke
 
@@ -52,12 +53,19 @@ app.post('/upvote-joke', (req, res) => {
     res.sendStatus(200)
 })
 
+// When user visits home page
 app.get('/', (req, res) => {
+
+    // Render leaderboard
+    // Pass in data
+    // jokes.chain().find({}).simplesort('upvotes').data().reverse()
+    // For jokes, chain functions: Find all, sort by upvotes, populate data in array, reverse order 
     res.render('leaderboard', {
         data: jokes.chain().find({}).simplesort('upvotes').data().reverse()
     })
 })
 
+// Start server on Port: 3000
 app.listen(3000, () => {
     console.log("Server is running")
 })
